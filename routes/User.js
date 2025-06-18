6c158a2603f1e9e5d708523d31a34cd9543ebefb
Zheneos <loic.scognamiglio16@gmail.com>
2025-06-18 17:10:50 +0200

    feat: nouvelle méthode pour sql + checkMail

--- routes/User.js
+++ routes/User.js
@@ -10,38 +10,32 @@ const db = new Database();
 
 router.post('/login', async (req, res) => {
   const { login, password } = req.body;
-  if (!login || !password) {
-    return res.status(400).send('Malformation');
-  }
 
+  
+  if (!login || !password) return res.status(400).send('Malformation');
+  
+
+  // Vérifie si le login existe
   const result = await db.query('SELECT password FROM login WHERE login = ?', login);
-  if (!result || result.length === 0) {
-    return res.status(401).send('Identifiants incorrects');
-  }
+  if (!result || result.length === 0) return res.status(401).send('Identifiants incorrects');
+  
 
+  // Vérifie si le mot de passe est bon
   const valid = await check(result[0].password, password);
-  if (!valid) {
-    return res.status(401).send('Identifiants incorrects');
-  }
-
+  if (!valid) return res.status(401).send('Identifiants incorrects');
+  
   return res.send("success");
 });
 
 router.post('/create', async (req, res) => {
   const { login, password, email } = req.body;
 
-  if (!login || !password || !email) {
-    return res.status(400).send('Malformation');
-  }
+  if (!login || !password || !email) return res.status(400).send('Malformation');
 
-  if (!validator.isEmail(email)) {
-    return res.status(400).send('Mail non conforme');
-  }
+  if (!validator.isEmail(email)) return res.status(400).send('Mail non conforme');
 
   const alreadyExist = await db.exist('SELECT 1 FROM login WHERE login = ? or email = ?', login, email);
-  if (alreadyExist) {
-    return res.status(409).send('alreadyExist');
-  }
+  if (alreadyExist) return res.status(409).send('alreadyExist');
 
   tokenMail = crypto.randomBytes(16).toString('hex').slice(0, 16);
 
@@ -59,6 +53,18 @@ router.post('/create', async (req, res) => {
     `<a href='${link}'>Clique-sur moi !</a><br><br> et si marche pas, accède directement à cette url <a href='${link}'>${link}</a>`
   );
 
+
+});
+
+router.get('/validMail/:email/:token', async (req, res) => {
+  const { email, token } = req.params;
+
+  const goodToken = await db.exist('delete FROM tokenMail WHERE mail = ? and token = ?', email, token);
+  if(!goodToken) return res.status(401).send('token/mail incorrects');
+
+  await db.exist('update login set status = ?', 1);
+
+  return res.send("success");
 
 });
 
