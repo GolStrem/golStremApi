0e0fbddfffca84f8a3f0c505b4dd19839dfeb4fe
Zheneos <loic.scognamiglio16@gmail.com>
2025-06-18 20:10:25 +0200

    feat: récupération et édition des infos user

--- routes/UserInfo.js
+++ routes/UserInfo.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const { isSubset } = require('../lib/Util');
+
 
 const session = new (require('../lib/Session'))();
 const db = new (require('../lib/DataBase'))();
@@ -10,6 +12,21 @@ router.get('', async (req, res) => {
     if(!await session.checkToken(authHeader, req.ip)) return res.status(401).send('token unknown');
     const userInfo = await session.getInfo();
     return res.send(userInfo ? userInfo : {});
+});
+
+router.put('', async (req, res) => {
+    const authHeader = req.headers['authorization'];
+    if(!await session.checkToken(authHeader, req.ip)) return res.status(401).send('token unknown');
+
+    const keyExist = ['theme','lastWorkspace','seriousMode']
+
+    const keys = Object.keys(req.body)
+
+    if(!isSubset(keys,keyExist)) return res.status(400).send('Malformation');
+
+
+    await session.updateInfo(keys, Object.values(req.body));
+    return res.send("success");
 });
 
 
